f26d823f6fb901040d3cad5bea81cf0297e63d85
doiken <doiken@users.noreply.github.com>
2019-11-04 01:13:06 +0900

    nested snippet

--- Source/Snippet.spoon/init.lua
+++ Source/Snippet.spoon/init.lua
@@ -18,7 +18,7 @@ obj.logger = hs.logger.new('Snippet')
 -- Internal variable
 obj._chooser = nil
 -- Internal variable - Table of "hs" action function
-obj._fnMap = {}
+obj._contentsMap = {}
 -- Internal variable - Cache for focused window to work around the current window losing focus after the chooser comes up
 obj._prevFocusedWindow = nil
 
@@ -27,10 +27,11 @@ obj._prevFocusedWindow = nil
 --- The format is bellow.
 --- { text = "TEXT", action = "ACTION_NAME", contents = "CONTENTS" }
 --- TEXT is index to choose snippet.
---- Action is "text" or "shell" or "hs".
+--- Action is "text" or "shell" or "hs" or "nest".
 --- When action is text, CONTENTS is directly used.
 --- When action is shell, CONTENTS is evaluated as shell script.
 --- When action is hs, CONTENTS is called as hammerspoon function.
+--- When action is nest, CONTENTS is table of nested snippets.
 --- e.g. 
 --- spoon.Snippet.snippets = {
 ---   {
@@ -48,42 +49,41 @@ obj._prevFocusedWindow = nil
 ---     action = "hs",
 ---     contents = function () hs.notify.show("title", "", "") end,
 ---   },
+---   {
+---     text = "nest greet",
+---     action = "nest",
+---     contents = {
+---       {
+---         text = "nested greeting",
+---         action = "text",
+---         contents = "nested Hello!",
+---       },
+---     },
+---   },
 --- }
 obj.snippets = {}
 
---- Snippet:init()
---- Method
---- initializer
----
---- Parameters:
----  * None
----
---- Returns:
----  * None
-function obj:init()
-  self._chooser = hs.chooser.new(hs.fnutils.partial(self._processSelectedItem, self))
-  self._chooser:choices(self:_populateChooser())
-  return obj
-end
+local actions = {
+   none = function() end,
+   text = function(v) return v.contents end,
+   shell = function(v) return hs.execute(v.contents) end,
+   hs = function(v)
+      local res = obj._contentsMap[v.contents]()
+      if type(res) == "string" then
+         return res
+      else
+         return
+      end
+   end,
+   nest = function(v) obj._contentsMap[v.contents]:show() end,
+}
 
-function obj:_processSelectedItem(value)
-   local actions = {
-      none = function() end,
-      text = function(v) return v.contents end,
-      shell = function(v) return hs.execute(v.contents) end,
-      hs = function(v)
-         local res = obj._fnMap[v.contents]()
-         if type(res) == "string" then
-            return res
-         else
-            return
-         end
-      end,
-   }
-   if self._prevFocusedWindow ~= nil then
-      self._prevFocusedWindow:focus()
+local function processSelectedItem(value)
+   if obj._prevFocusedWindow ~= nil then
+      obj._prevFocusedWindow:focus()
    end
    if value and type(value) == "table" then
+      obj.logger:e(hs.inspect(value))
       if value.action and actions[value.action] then
         contents = actions[value.action](value)
         if contents then
@@ -96,25 +96,58 @@ function obj:_processSelectedItem(value)
    end
 end
 
-function obj:_populateChooser()
+local prefix_no = 1
+local function prepareSnippets(snippets)
+   local prepareContentsByAction = {
+      hs = function (v) return v.contents end,
+      nest = function (v)
+         local chooser = hs.chooser.new(hs.fnutils.partial(processSelectedItem))
+         chooser:choices(prepareSnippets(v.contents))
+         return chooser
+      end,
+   }
    local menuData = {}
-
-   for _,v in ipairs(self.snippets) do
-      if v.action == "hs" then
-         local hash = hs.hash.MD5(debug.getinfo(v.contents, "S").source)
-         self._fnMap[hash] = v.contents
+   for _,v in ipairs(snippets) do
+      local f = prepareContentsByAction[v.action]
+      -- action, which needs prepare, stores processed contents into _contentsMap,
+      -- and overwrite original contents by hash to indicate key of _contentsMap.
+      if f then
+         local hash = hs.hash.MD5(prefix_no .. v.text)
+         local contents = f(v)
+         obj._contentsMap[hash] = contents
          v.contents = hash
+         prefix_no = prefix_no + 1
       end
       table.insert(menuData, v)
    end
+   return menuData
+end
+
+local function populateChooser()
+   local menuData = prepareSnippets(obj.snippets)
    if #menuData == 0 then
       table.insert(menuData, { text="",
                                subText="《Snippet is empty》",
                                action = 'none',
                                image = hs.image.imageFromName('NSCaution')})
    end
-   self.logger.df("Returning menuData = %s", hs.inspect(menuData))
+   obj.logger.df("Returning menuData = %s", hs.inspect(menuData))
    return menuData
+end
+
+--- Snippet:init()
+--- Method
+--- initializer
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function obj:init()
+  self._chooser = hs.chooser.new(hs.fnutils.partial(processSelectedItem))
+  self._chooser:choices(populateChooser())
+  return obj
 end
 
 --- Snippet:showSnippet()
